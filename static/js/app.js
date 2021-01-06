f9f80f3273029b5318d0d5d1fb0d7ac7df47f409
Cole Comstock <cole.d.comstock@gmail.com>
2021-01-05 20:56:57 -0700

    Fixed bar chart, got the OTU lables showing along the axis

--- static/js/app.js
+++ static/js/app.js
@@ -1,13 +1,14 @@
-// Capture Sample Selection ID
 
 // Go through JSON Data
 d3.json("/data/samples.json").then(function(samples) {
     // console.log(samples); 
-    var sampleIDs = samples.samples.map(person => person.id); 
-    var otuIDs = samples.samples.map(person => person.otu_ids); 
+    var allSampleData = samples.samples.map(person => person);
+    
+    var sampleMetadata = samples.metadata.map(item => item); 
+    console.log(allSampleData);
 
-    for (var i = 0; i < sampleIDs.length; i++) {
-        d3.select("#selDataset").append("option").text(sampleIDs[i])
+    for (var i = 0; i < allSampleData.length; i++) {
+        d3.select("#selDataset").append("option").text(allSampleData[i].id)
     }
 
     buildDashboard();
@@ -21,45 +22,46 @@ d3.json("/data/samples.json").then(function(samples) {
         
         var sampleMetadata = samples.metadata.map(person => person);
 
-        var sampleData = samples.samples.map(data => data)
-        console.log(sampleData);
+        for (var i = 0; i < allSampleData.length; i++) {
 
-        for (var i = 0; i < sampleIDs.length; i++) {
-
-            if (sampleSelection === sampleIDs[i]) {
+            if (sampleSelection === allSampleData[i].id) {
                 summaryTable.append("p").append('strong').text(`ID: ${sampleSelection}`);
                 summaryTable.append('p').append('strong').text(`Ethnicity: ${sampleMetadata[i].ethnicity}`);
                 summaryTable.append('p').append('strong').text(`Gender: ${sampleMetadata[i].gender}`);
                 summaryTable.append('p').append('strong').text(`Age: ${sampleMetadata[i].age}`);
                 summaryTable.append('p').append('strong').text(`Location: ${sampleMetadata[i].location}`);
                 summaryTable.append('p').append('strong').text(`BB Type: ${sampleMetadata[i].bbtype}`);
-                summaryTable.append('p').append('strong').text(`wfreq: ${sampleMetadata[i].wfreq}`);
+                summaryTable.append('p').append('strong').text(`wfreq: ${sampleMetadata[i].wfreq}`); 
+                
+                var sampleValues = allSampleData[i].sample_values.slice(0, 10);
+                var reversedSampleValues = sampleValues.reverse(); 
+                var sampleOTUids = allSampleData[i].otu_ids.slice(0, 10);
+                var reversedOTUs = sampleOTUids.reverse();
+                var reverseOTUsString = reversedOTUs.toString();
+                
+                var otuLabels = []; 
 
-                console.log(sampleData[i].id); 
+                reversedOTUs.forEach((label) => otuLabels.push(`OTU ${label}`)) 
 
                 var trace1 = {
-                    x: sampleData[i].otu_ids,
-                    y: sampleData[i].sample_values,
-                    type: 'bar'
-                }
+                    type: 'bar',
+                    x: sampleValues,
+                    y: otuLabels,
+                    orientation: 'h'
+                  };
             }
         }
 
-        // var trace1 = {
-        //     x: sampleData.sample_values, 
-        //     y: sampleData.otu_ids,
-        //     type: 'bar'
-        // }
-
         // Create the data array for the plot
-        var data = [trace1];
+        var data = [trace1]; 
 
         // Define the plot layout
         var layout = {
-        title: "Square Root of Cancer Survival by Organ",
-        xaxis: { title: "OTU ID" },
-        yaxis: { title: "Count" }
-        };
+            xaxis: {
+                showticklabels: true,
+                tickmode: 'auto'
+            }
+        }
 
         // Plot the chart to a div tag with id "plot"
         Plotly.newPlot("bar", data, layout);
